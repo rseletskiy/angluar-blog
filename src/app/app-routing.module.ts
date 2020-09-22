47b3fafe562c571ccdaaafa238d960d4655870ad
roman <Soir_@ukr.net>
2020-09-22 13:19:00 +0300

    admin layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { PostPageComponent } from './post-page/post-page.component';
 import { MainLaoutComponent } from './shared/components/main-laout/main-laout.component';
@@ -9,11 +9,18 @@ const routes: Routes = [
     {path: '', redirectTo: '/', pathMatch: 'full'},
     {path: '', component: HomePageComponent},
     {path: 'post/:id', component: PostPageComponent}
-  ]}
+  ]},
+  {
+    path: 'admin', loadChildren: () => {
+      return import('./admin/admin.module').then(m => m.AdminModule);
+    }
+  }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: PreloadAllModules
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
